67126279fa634732eeabbbf3796cd584a3b1fe78
Brian Woods <me@brian.biz>
2021-06-12 07:00:14 -0700

    compiles

--- contracts/Estate.sol
+++ contracts/Estate.sol
@@ -2,28 +2,30 @@ pragma solidity ^0.5.0;
 
 import "./ERC721Tradable.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
 
 /**
  * @title Estate
  * Estate - a contract for my non-fungible creatures.
  */
-contract Estate is ERC721Tradable {
+contract Estate is Ownable, ERC721Tradable {
     constructor(address proxyRegistryAddress)
         public
         ERC721Tradable("Estate", "BNFT", proxyRegistryAddress)
     {}
 
-    function verify(bytes32 data, bytes32 signature) pure returns (bool) {
-        return keccack256(data)
-            .toEthSignedMessageHash()
-            .recover(signature) == owner;
-    }
-
-    function canMint(bytes32 signature, uint blockNo, uint32 topLeftY, uint32 topLeftX32, uint32 bottomRightY, uint32 bottomRightX) public view returns (bool) {
+    function canMint(bytes memory signature, uint blockNo, uint32 topLeftX, uint32 topLeftY, uint32 bottomRightX, uint32 bottomRightY) public view returns (bool) {
         require (block.number <= blockNo + 20);
-
-        data = keccak256(abi.encodePacked(msg.sender, blockNo, topLeftX, topLeftY, bottomRightX, bottomRightY));
-        return _verify(data, signature);
+        
+        bytes memory data;
+        bytes32 convertedData;
+        
+        data = abi.encodePacked(msg.sender, blockNo, topLeftX, topLeftY, bottomRightX, bottomRightY);
+        convertedData = keccak256(data);
+        convertedData = ECDSA.toEthSignedMessageHash(convertedData);
+        address recoveredOwner = ECDSA.recover(convertedData, signature);
+        
+        return recoveredOwner == owner();
 
         //XXX: todo - fees!
     }
