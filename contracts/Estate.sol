267ac97be788b43631fa4a041d2f255837ea7c72
Brian Woods <me@brian.biz>
2021-06-14 08:16:58 -0700

    Working signatures and data encoding

--- contracts/Estate.sol
+++ contracts/Estate.sol
@@ -9,22 +9,42 @@ import "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
  * Estate - a contract for my non-fungible creatures.
  */
 contract Estate is Ownable, ERC721Tradable {
+    event DebugSignature(bytes data);
+    event DebugBytes(bytes data);
+    event DebugBytes32(bytes32 data);
+    event RecoveredOwner(address owner);
+    event EmitAddress(address addy);
+    event Length(uint length);
+    event Msg(string msg);
+    
     constructor(address proxyRegistryAddress)
         public
         ERC721Tradable("Estate", "BNFT", proxyRegistryAddress)
     {}
+    
+    //function mintTo(address to, bytes memory signature, uint blockNo, uint32 topLeftX, uint32 topLeftY, uint32 bottomRightX, uint32 bottomRightY) public onlyOwner {
+    //    if (canMint(to, signature, blockNo, topLeftX, topLeftY, bottomRightX, bottomRightY)) {
+    //        super.mintTo(to);
+    //    }
+    //}
 
-    function canMint(bytes memory signature, uint blockNo, uint32 topLeftX, uint32 topLeftY, uint32 bottomRightX, uint32 bottomRightY) public view returns (bool) {
-        require (block.number <= blockNo + 20);
+    using ECDSA for bytes32;
+
+    function canMint(address to, bytes memory signature, uint blockNo, uint32 topLeftX, uint32 topLeftY, uint32 bottomRightX, uint32 bottomRightY) public returns (bool) {
+        //require (block.number <= blockNo + 20);
         
         bytes memory data;
         bytes32 convertedData;
         
-        data = abi.encodePacked(msg.sender, blockNo, topLeftX, topLeftY, bottomRightX, bottomRightY);
+        data = abi.encodePacked(to, blockNo, topLeftX, topLeftY, bottomRightX, bottomRightY);
+        emit DebugBytes(data);
         convertedData = keccak256(data);
-        convertedData = ECDSA.toEthSignedMessageHash(convertedData);
-        address recoveredOwner = ECDSA.recover(convertedData, signature);
-        
+        emit DebugBytes32(convertedData);
+        convertedData = convertedData.toEthSignedMessageHash();
+        emit DebugBytes32(convertedData);
+        address recoveredOwner = convertedData.recover(signature);
+        emit RecoveredOwner(recoveredOwner);
+
         return recoveredOwner == owner();
 
         //XXX: todo - fees!
